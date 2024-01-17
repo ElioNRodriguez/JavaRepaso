9800b4ad0e657a62d302ff00a5ff40e19a5f443c
Elio <Elio@Elio-PC>
2024-01-16 21:44:55 -0300

    Clase Producto

--- Repaso1/src/producto/dominio/Producto.java
+++ Repaso1/src/producto/dominio/Producto.java
@@ -1,5 +1,63 @@
 package producto.dominio;
 
 public class Producto {
+	
+	Integer id;
+	String descripcion;
+	Integer cantidad;
+	Double precio;
+	Boolean disponible;
+	
+	public Producto(Integer id, String descripcion, Integer cantidad, Double precio, Boolean disponible) {
+		super();
+		this.id = id;
+		this.descripcion = descripcion;
+		this.cantidad = cantidad;
+		this.precio = precio;
+		this.disponible = disponible;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Integer getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public Double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+
+	public Boolean getDisponible() {
+		return disponible;
+	}
+
+	public void setDisponible(Boolean disponible) {
+		this.disponible = disponible;
+	}
+	
+	
+	
 
 }
